d09a4d2d638a59d5f28ca52dee9d3a87a79014e9
Hamza <33000412+MeeHamza@users.noreply.github.com>
2026-02-17 10:34:28 +0000

    app.js update

--- app.js
+++ app.js
@@ -11,18 +11,18 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, function(err) {
+mongoose.connect(
+//   "mongodb+srv://superuser:SuperPassword@supercluster.d83jj.mongodb.net/superData",
+process.env.MONGO_URI,
+  function (err) {
     if (err) {
-        console.log("error!! " + err)
+      console.log("error!! " + err);
     } else {
-      //  console.log("MongoDB Connection Successful")
+      console.log("MongoDB Connection Successful");
     }
-})
+  }
+);
+
 
 var Schema = mongoose.Schema;
 
